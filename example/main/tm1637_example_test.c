d186ce60de235862f481f39c3b24e72d8de03682
Hays Chan <25737801+hayschan@users.noreply.github.com>
2024-01-15 15:57:21 +0800

    Change max6675 example to tm1637

---
+++ example/main/tm1637_example_test.c
@@ -0,0 +1,23 @@
+#include "tm1637.h"
+
+#define CLK_PIN GPIO_NUM_17
+#define DIO_PIN GPIO_NUM_16
+
+void app_main(void)
+{
+    TM1637 tm1637(CLK_PIN, DIO_PIN);
+
+    tm1637.init();
+    tm1637.set(BRIGHTEST);
+    tm1637.point(POINT_ON);
+
+    // display tm1637 four digit, from left to right, 0000 - 9999
+    for (int i = 0; i < 10; i++)
+    {
+        tm1637.display(0, i);
+        tm1637.display(1, i);
+        tm1637.display(2, i);
+        tm1637.display(3, i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
